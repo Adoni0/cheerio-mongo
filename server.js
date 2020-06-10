437c6b8d3a667aa239cb8d9b8dc0983e83482457
Adam SOudani <asoudani@email.com>
2020-06-10 12:20:02 -0700

    routes setup

--- server.js
+++ server.js
@@ -15,7 +15,39 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/unit18Populater", {
+  useNewUrlParser: true,
+});
 
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.theonion.com/").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    //title class = 'sc-1qoge05-0'
+    //summary class = 'sc-1d3a351-0'
+    //
+
+    $("article").each(function (i, element) {
+      var result = {};
+
+      result.title = $(this).children("h4").class("sc-1qoge05-0");
+      //$('h4.sc-1qoge05-0')
+      result.summary = $(this).children("p").class("sc-1d3a351-0");
+      //$('p.sc-1d3a351-0')
+      result.link = $(this).children("a").attr("href");
+    });
+  });
+});
+
+app.get("/articles", function (req, res) {});
+
+//grab specific article by id and and populate with comments
+app.get("/articles/:id", function (req, res) {});
+
+app.post("/articles/:id", function (req, res) {});
+
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
